5be699d3923e856f6eaa64f6c24f9b30bb0b8072
DaVinCi <Fox.winner9@gmail.com>
2024-05-09 15:12:50 +0300

    change search bar to search by word in search place builder

--- app/src/main/java/ru/nn/tripnn/ui/screen/main/search/SearchPlaceBuilderScreen.kt
+++ app/src/main/java/ru/nn/tripnn/ui/screen/main/search/SearchPlaceBuilderScreen.kt
@@ -22,9 +22,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -35,8 +40,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,9 +56,9 @@ import ru.nn.tripnn.domain.SearchFilters
 import ru.nn.tripnn.ui.common.CatalogNavigation
 import ru.nn.tripnn.ui.common.MontsText
 import ru.nn.tripnn.ui.common.PrimaryButton
-import ru.nn.tripnn.ui.common.Search
 import ru.nn.tripnn.ui.theme.TripNNTheme
 import ru.nn.tripnn.ui.theme.TripNnTheme
+import ru.nn.tripnn.ui.theme.montserratFamily
 import java.time.LocalTime
 
 @Composable
@@ -113,7 +124,7 @@ fun SearchPlaceBuilderScreen(
                 chosen = chosenCategory
             )
 
-            Search(modifier = Modifier.fillMaxWidth(), onSearch = { searchInput = it })
+            FilterByWord(text = searchInput, onChange = { searchInput = it })
 
             PriceChoice(picked = pickedPrices, onPick = { pickedPrices[it] = !pickedPrices[it] })
 
@@ -169,6 +180,78 @@ fun SearchPlaceBuilderScreen(
                     toResultScreen()
                 }
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilterByWord(text: String, onChange: (String) -> Unit) {
+    Column {
+        MontsText(
+            text = stringResource(id = R.string.filter_by_word),
+            style = MaterialTheme.typography.displayMedium
+        )
+
+        Spacer(modifier = Modifier.height(13.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(5.dp))
+                .background(TripNnTheme.colorScheme.fieldBackground)
+        ) {
+            BasicTextField(
+                value = text,
+                onValueChange = {
+                    if (it.length <= 20) {
+                        onChange(it)
+                    }
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        this.defaultKeyboardAction(ImeAction.Done)
+                    }
+                ),
+                singleLine = true,
+                textStyle = TextStyle(
+                    color = TripNnTheme.colorScheme.textColor,
+                    fontSize = 13.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = montserratFamily,
+                    textAlign = TextAlign.Start
+                )
+            ) { innerTextField ->
+                TextFieldDefaults.DecorationBox(
+                    value = text,
+                    visualTransformation = VisualTransformation.None,
+                    innerTextField = innerTextField,
+                    singleLine = true,
+                    enabled = true,
+                    placeholder = {
+                        MontsText(
+                            text = stringResource(id = R.string.search),
+                            style = MaterialTheme.typography.labelMedium,
+                            color = TripNnTheme.colorScheme.hint,
+                        )
+                    },
+                    interactionSource = remember { MutableInteractionSource() },
+                    contentPadding = PaddingValues(
+                        horizontal = 15.dp,
+                        vertical = 7.dp
+                    ),
+                    shape = RoundedCornerShape(5.dp),
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        focusedContainerColor = TripNnTheme.colorScheme.fieldBackground,
+                        unfocusedContainerColor = TripNnTheme.colorScheme.fieldBackground,
+                        focusedPlaceholderColor = TripNnTheme.colorScheme.hint,
+                        unfocusedPlaceholderColor = TripNnTheme.colorScheme.hint
+                    )
+                )
+            }
         }
     }
 }
