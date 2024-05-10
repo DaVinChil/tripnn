27709a11b8d98140c3f0dc878d98e8664f8d7a61
DaVinCi <Fox.winner9@gmail.com>
2024-05-10 16:00:42 +0300

    make placeid unique in places history database

--- app/src/main/java/ru/nn/tripnn/data/local/history/place/HistoryPlace.kt
+++ app/src/main/java/ru/nn/tripnn/data/local/history/place/HistoryPlace.kt
@@ -1,9 +1,10 @@
 package ru.nn.tripnn.data.local.history.place
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "places_history")
+@Entity(tableName = "places_history", indices = [Index(value = ["placeId"], unique = true)])
 data class HistoryPlace (
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
